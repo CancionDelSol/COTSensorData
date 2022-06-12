6b01d18d50a6f03a47e56f46266f144ee7fdba1b
Roger Johnson <rkj8@students.uwf.edu>
2022-06-12 02:25:34 +0000

    stenciling out the classes

--- src/interfaces/arduino/BTTranceiver.java
+++ src/interfaces/arduino/BTTranceiver.java
@@ -1,5 +1,19 @@
 package interfaces.arduino;
 
 public class BTTranceiver {
+    //region Fields
+
+    //endregion
+
+    //region Properties
+
+    //endregion
+
+    //region Constructor
     
+    //endregion
+
+    //region Methods
+
+    //endregion
 }
--- src/interfaces/arduino/WifiTransceiver.java
+++ src/interfaces/arduino/WifiTransceiver.java
@@ -1,5 +1,19 @@
 package interfaces.arduino;
 
 public class WifiTransceiver {
+    //region Fields
+
+    //endregion
+
+    //region Properties
+
+    //endregion
+
+    //region Constructor
     
+    //endregion
+
+    //region Methods
+
+    //endregion
 }
