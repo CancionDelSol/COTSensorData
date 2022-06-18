5b400fbf8295795842ca0f3df7a333f66807f613
Roger Johnson <rkj8@students.uwf.edu>
2022-06-18 16:23:28 +0000

    Added config default files. Now I need to populate them

--- src/util/ConfigurableBase.java
+++ src/util/ConfigurableBase.java
@@ -1,8 +1,21 @@
 package util;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
 import interfaces.IConfigurable;
+import interfaces.callbacks.IErrorCallback;
 
 /**
  * The base class for a configurable
@@ -49,12 +62,50 @@ public class ConfigurableBase implements IConfigurable {
     }
 
     // Load Configuration from file
-    public void LoadConfiguration(String configPath) {
+    public boolean LoadConfiguration(String configPath, IErrorCallback onError) {
+        // Use the configPath as filename
+        File fXmlFile = new File(configPath);
+
+        // Document builder factory
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+
+        try {
+            // Document builder to parse
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+
+            // Parse XML document
+            Document doc = dBuilder.parse(fXmlFile);
+
+            // Normalize head element
+            doc.getDocumentElement().normalize();
+            
+            // Get the setting elements
+            NodeList nodeList = doc.getElementsByTagName("setting");
+
+            // Iterate through the nodes
+            //  and save the 
+
+        } catch (ParserConfigurationException | SAXException | IOException exc) {
+            // Log exception
+            Logger.Error("Parsing error: " + exc.getMessage());
+
+            // Callback
+            try {
+                if (onError != null) 
+                    onError.onError();
+            } catch (Exception onErrorException) {
+                Logger.Error("Error occurred in callback: " + onErrorException.getMessage());
+            }
+
+            // Return failure
+            return false;
+        }
         
+        return true;
     }
 
     // Save Configuration to file
-    public void SaveConfiguration(String configPath) {
+    public boolean SaveConfiguration(String configPath, IErrorCallback onError) {
 
     }
     //endregion
---
+++ src/util/Globals.java
@@ -0,0 +1,16 @@
+package util;
+
+/**
+ * Make some constants globally accessible
+ */
+public class Globals {
+    //region Constants
+    public static final String MAIN_CONFIG_FILE = "MainConfig.cfg";
+    public static final String ESP_MODULE_CONFIG_FILE = "ESPConfig.cfg";
+    public static final String ARDUINO_CONFIG_FILE = "ArdConfig.cfg";
+    public static final String LOPAN_MODULE_CONFIG_FILE = "LoPanModule.cfg";
+    public static final String SIX_LOWPAN_PROTO_CONFIG_FILE = "6LoWPANProtoConfig.cfg";
+    public static final String WIFI_PROTO_CONFIG_FILE = "WifiProtoConfig.cfg";
+    public static final String BLUETOOTH_PROTO_CONFIG_FILE = "BluetoothProtoConfig.cfg";
+    //endregion
+}
