d789c5c2a3ce8067b77a893a9d2d4d6d441a31f4
Roger Johnson <rkj8@students.uwf.edu>
2022-07-14 16:37:15 +0000

    Adding more information to the results class

--- src/telemetry/RoundTripResult.java
+++ src/telemetry/RoundTripResult.java
@@ -10,11 +10,24 @@ import interfaces.IEncryptionAlg;
  *  be instantiated until a "trip"
  *  is fully completed. For use with
  *  report creation
+ * 
+ * Properties:
+ *  - TotalDuration
+ *  - ProcedureStartTimeStamp
+ *  - RequestSentFromTransTimeStamp
+ *  - RequestRecByRecTimeStamp
+ *  - RespSentByRecTimeStamp
+ *  - RespRecByTransTimeStamp
  */
 public class RoundTripResult {
     
     //region Fields
     private long _duration;
+    private long _procStartTimeStamp;
+    private long _reqSentFromTransTimeStamp;
+    private long _reqRecByRecTimeStamp;
+    private long _respSentByRecTimeStamp;
+    private long _respRecByTransTimeStamp;
     private String _commProtoName;
     private String _encAlgName;
     private String _serverMessage;
@@ -34,10 +47,19 @@ public class RoundTripResult {
      * Main constructor
      */
     public RoundTripResult(long duration,
+                            long procStartTimeStamp,
+                            long reqSentFromTransTimeStamp,
+                            long reqRecByRecTimeStamp,
+                            long respSentByRecTimeStamp,
                             ICommProto protocolUsed,
                             IEncryptionAlg encryptionAlgUsed,
                             String status) {
+
         _duration = duration;
+        _procStartTimeStamp = procStartTimeStamp;
+        _reqSentFromTransTimeStamp = reqSentFromTransTimeStamp;
+        _reqRecByRecTimeStamp = reqRecByRecTimeStamp;
+        _respSentByRecTimeStamp = respSentByRecTimeStamp;
         _commProtoName = protocolUsed.getName();
         _encAlgName = encryptionAlgUsed.getName();
         _status = status;
@@ -53,6 +75,21 @@ public class RoundTripResult {
             .append('\n')
             .append(" Duration: ")
             .append(_duration)
+            .append('\n')
+            .append(" Start: ")
+            .append(_procStartTimeStamp)
+            .append('\n')
+            .append(" SentFromTrans: ")
+            .append(_reqSentFromTransTimeStamp)
+            .append('\n')
+            .append(" RecByReceiver: ")
+            .append(_reqRecByRecTimeStamp)
+            .append('\n')
+            .append(" RespSentByReceiver: ")
+            .append(_respSentByRecTimeStamp)
+            .append('\n')
+            .append(" RespRecByTransmitter: ")
+            .append(_respRecByTransTimeStamp)
             .append('\n')
             .append(" Status: ")
             .append(_status)
