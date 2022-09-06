ae68885d56a6963d518ae5848224a14e7790e1cb
Roger Johnson <rkj8@students.uwf.edu>
2022-09-05 22:06:21 -0500

    Should not be commented out

--- BTServer/pushToESP.sh
+++ BTServer/pushToESP.sh
@@ -2,5 +2,5 @@ SKETCH=./BTServer.ino
 BOARD=esp32:esp32:esp32
 PORT=/dev/ttyUSB1
 
-#arduino-cli compile -b ${BOARD} ${SKETCH}
+arduino-cli compile -b ${BOARD} ${SKETCH}
 arduino-cli upload -b ${BOARD} -p ${PORT} ${SKETCH} 
